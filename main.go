ebe56b494acb8c4e1e045b73ef5fd2d466dd0707
Mohammad Khafid Barokum <khafid@majoo.id>
2021-02-24 23:23:58 +0700

    add some example

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -34,10 +35,74 @@ func lessonTwo() {
 	x, y := <-c, <-c // receive from c
 
 	fmt.Println(x, y, x+y)
-	fmt.Println(":len(s)/2", s[:len(s)/2])
-	fmt.Println("len(s)/2:", s[len(s)/2:])
+}
+
+func blockingChannel() {
+	c := make(chan bool)
+	c <- true
+	fmt.Println("This line will never be printed")
+}
+
+func blockingChannelCont() {
+	c := make(chan bool)
+	go func() {
+		<-c
+	}()
+	c <- true
+	fmt.Println("This line is printed")
+}
+
+func bufferedChannel() {
+	c := make(chan bool, 5)
+	c <- true
+	fmt.Println("This line is printed")
+}
+
+func anonymous() {
+	for _, v := range []int{1, 2, 3, 4, 5} {
+		go func(i int) {
+			fmt.Println(i)
+		}(v)
+	}
+}
+
+func selectExample() {
+	c := make(chan int)
+
+	go func() {
+		<-time.After(time.Duration(rand.Intn(2)) * time.Second)
+		c <- 10
+	}()
+
+	select {
+	case val := <-c:
+		fmt.Println(val)
+	case <-time.After(time.Duration(rand.Intn(2)) * time.Second):
+		fmt.Println("timeout")
+	}
+}
+
+func defaultSelection() {
+	tick := time.Tick(100 * time.Millisecond)
+	fmt.Println("tick:", tick)
+	boom := time.Tick(500 * time.Millisecond)
+	fmt.Println("boom:", boom)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+			fmt.Println("<-tick: ", <-tick)
+		case <-boom:
+			fmt.Println("BOOM!")
+			fmt.Println("<-boom: ", <-boom)
+			return
+		default:
+			fmt.Println("   .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
 
 func main() {
-	lessonTwo()
+	defaultSelection()
 }
